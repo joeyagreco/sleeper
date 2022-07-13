303a1ff33795b1096071cbffafb81c66343c217a
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-13 14:35:09 -0500

    updated user flow

--- sleeper/api/UserAPIClient.py
+++ sleeper/api/UserAPIClient.py
@@ -5,13 +5,25 @@ from sleeper.util.ConfigReader import ConfigReader
 
 class UserAPIClient(APIClient):
     __USER_ROUTE = ConfigReader.get("api", "user_route")
+    __USERS_ROUTE = ConfigReader.get("api", "users_route")
+    __LEAGUE_ROUTE = ConfigReader.get("api", "league_route")
 
     @staticmethod
     def __build_user_object(user_dict: dict) -> User:
-        return User(username=user_dict["username"],
+        return User(username=user_dict.get("username", None),
                     user_id=user_dict["user_id"],
                     display_name=user_dict["display_name"],
-                    avatar=user_dict["avatar"])
+                    avatar=user_dict["avatar"],
+                    is_owner=user_dict.get("is_owner", None),
+                    is_bot=user_dict.get("is_bot", None),
+                    league_id=user_dict.get("league_id", None))
+
+    @classmethod
+    def __build_users_list(cls, user_dict_list: dict) -> list[User]:
+        users = list()
+        for user_dict in user_dict_list:
+            users.append(cls.__build_user_object(user_dict))
+        return users
 
     @classmethod
     def get_user(cls, *, username: str = None, user_id: str = None) -> User:
@@ -20,3 +32,8 @@ class UserAPIClient(APIClient):
         user_arg = username if username is not None else user_id
         url = cls._build_route(cls.__USER_ROUTE, f"{user_arg}")
         return cls.__build_user_object(cls._get(url))
+
+    @classmethod
+    def get_users_in_league(cls, *, league_id: str) -> list[User]:
+        url = cls._build_route(cls.__LEAGUE_ROUTE, league_id, cls.__USERS_ROUTE)
+        return cls.__build_users_list(cls._get(url))
--- sleeper/model/User.py
+++ sleeper/model/User.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass(kw_only=True)
 class User:
-    username: str
+    username: Optional[str]
     user_id: str
     display_name: str
     avatar: str
+    league_id: Optional[str]
+    is_owner: Optional[bool]
+    is_bot: Optional[bool]
