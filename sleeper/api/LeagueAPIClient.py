2a08b455f19ab08acc1408bcd6500bcc3278af22
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-13 21:08:23 -0500

    moved build method to class

--- sleeper/api/LeagueAPIClient.py
+++ sleeper/api/LeagueAPIClient.py
@@ -61,14 +61,6 @@ class LeagueAPIClient(APIClient):
         return matchups
 
     @classmethod
-    def __build_from_playoff_matchup_object(cls, from_playoff_matchup_object: Optional[dict]) -> Optional[
-        FromPlayoffMatchup]:
-        if from_playoff_matchup_object is None:
-            return None
-        return FromPlayoffMatchup(won_matchup_id=from_playoff_matchup_object.get("w", None),
-                                  lost_matchup_id=from_playoff_matchup_object.get("l", None))
-
-    @classmethod
     def __build_playoff_matchup_object(cls, playoff_matchup_object: dict) -> PlayoffMatchup:
         return PlayoffMatchup(round=playoff_matchup_object["r"],
                               matchup_id=playoff_matchup_object["m"],
@@ -76,10 +68,8 @@ class LeagueAPIClient(APIClient):
                               team_2_roster_id=playoff_matchup_object["t2"],
                               winning_roster_id=playoff_matchup_object["w"],
                               losing_roster_id=playoff_matchup_object["l"],
-                              team_1_from=cls.__build_from_playoff_matchup_object(
-                                  playoff_matchup_object.get("t1_from", None)),
-                              team_2_from=cls.__build_from_playoff_matchup_object(
-                                  playoff_matchup_object.get("t2_from", None)))
+                              team_1_from=FromPlayoffMatchup.from_dict(playoff_matchup_object.get("t1_from", None)),
+                              team_2_from=FromPlayoffMatchup.from_dict(playoff_matchup_object.get("t2_from", None)))
 
     @classmethod
     def __build_playoff_matchups_list(cls, playoff_matchup_dict_list: dict) -> list[PlayoffMatchup]:
--- sleeper/model/FromPlayoffMatchup.py
+++ sleeper/model/FromPlayoffMatchup.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from typing import Optional
 
@@ -6,3 +8,10 @@ from typing import Optional
 class FromPlayoffMatchup:
     won_matchup_id: Optional[int]
     lost_matchup_id: Optional[int]
+
+    @staticmethod
+    def from_dict(from_playoff_matchup_object: Optional[dict]) -> Optional[FromPlayoffMatchup]:
+        if from_playoff_matchup_object is None:
+            return None
+        return FromPlayoffMatchup(won_matchup_id=from_playoff_matchup_object.get("w", None),
+                                  lost_matchup_id=from_playoff_matchup_object.get("l", None))
