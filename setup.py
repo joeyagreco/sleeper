f5d18f0555b44796e13e8f4593b103ae72115d51
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-13 22:03:03 -0500

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE") as f:
 
 setuptools.setup(
     name="sleeper",
-    version="0.2.0",
+    version="0.3.0",
     author="Joey Greco",
     author_email="joeyagreco@gmail.com",
     description="A Python wrapper for the Sleeper API.",
--- sleeper/__init__.py
+++ sleeper/__init__.py
@@ -1,3 +1,3 @@
 from .api import UserAPIClient, LeagueAPIClient
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
