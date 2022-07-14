1d2c51451b4a995be2e67ad3e0dbbe4849b0aa14
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-13 20:59:10 -0500

    moved build method to class

--- sleeper/api/UserAPIClient.py
+++ sleeper/api/UserAPIClient.py
@@ -8,21 +8,11 @@ class UserAPIClient(APIClient):
     __USERS_ROUTE = ConfigReader.get("api", "users_route")
     __LEAGUE_ROUTE = ConfigReader.get("api", "league_route")
 
-    @staticmethod
-    def __build_user_object(user_dict: dict) -> User:
-        return User(username=user_dict.get("username", None),
-                    user_id=user_dict["user_id"],
-                    display_name=user_dict["display_name"],
-                    avatar=user_dict["avatar"],
-                    is_owner=user_dict.get("is_owner", False),
-                    is_bot=user_dict.get("is_bot", False),
-                    league_id=user_dict.get("league_id", None))
-
     @classmethod
     def __build_users_list(cls, user_dict_list: dict) -> list[User]:
         users = list()
         for user_dict in user_dict_list:
-            users.append(cls.__build_user_object(user_dict))
+            users.append(User.from_dict(user_dict))
         return users
 
     @classmethod
@@ -31,7 +21,7 @@ class UserAPIClient(APIClient):
             raise ValueError("Must pass 'username' or 'user_id'.")
         user_arg = username if username is not None else user_id
         url = cls._build_route(cls.__USER_ROUTE, f"{user_arg}")
-        return cls.__build_user_object(cls._get(url))
+        return User.from_dict(cls._get(url))
 
     @classmethod
     def get_users_in_league(cls, *, league_id: str) -> list[User]:
--- sleeper/model/User.py
+++ sleeper/model/User.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from typing import Optional
 
@@ -11,3 +13,14 @@ class User:
     league_id: Optional[str]
     is_owner: Optional[bool]
     is_bot: Optional[bool]
+
+    @staticmethod
+    def from_dict(user_dict: dict) -> User:
+        return User(username=user_dict.get("username", None),
+                    user_id=user_dict["user_id"],
+                    display_name=user_dict["display_name"],
+                    avatar=user_dict["avatar"],
+                    is_owner=user_dict.get("is_owner", False),
+                    is_bot=user_dict.get("is_bot", False),
+                    league_id=user_dict.get("league_id", None))
+
