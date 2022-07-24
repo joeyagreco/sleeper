36d355ae1bd2d1bf2f329f64de5340646dd9e1ae
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-23 20:01:16 -0500

    added test

--- test/test_api/test_AvatarAPIClient.py
+++ test/test_api/test_AvatarAPIClient.py
@@ -40,6 +40,25 @@ class TestAvatarAPIClient(unittest.TestCase):
             self.assertTrue(os.path.exists(fullImagePath))
 
     @mock.patch("requests.get")
+    def test_get_avatar_thumbnail_is_true_happy_path(self, mock_requests_get):
+        mock_dict = {}
+        with open(os.path.join(self.PATH_TO_TEST_DIR, "test.png"), "rb") as image:
+            f = image.read()
+            original_image_bytes = bytearray(f)
+        mock_response = MockResponse(mock_dict, 200, content=original_image_bytes)
+        mock_requests_get.return_value = mock_response
+
+        with tempfile.TemporaryDirectory() as tempDir:
+            fullImagePath = os.path.join(tempDir, "tmp.png")
+            AvatarAPIClient.get_avatar(avatar_id="avatar_id", save_to_path=fullImagePath, thumbnail=True)
+
+            with open(fullImagePath, "rb") as image:
+                f = image.read()
+                saved_image_bytes = bytearray(f)
+            self.assertEqual(original_image_bytes, saved_image_bytes)
+            self.assertTrue(os.path.exists(fullImagePath))
+
+    @mock.patch("requests.get")
     def test_get_avatar_avatar_not_found_raises_exception(self, mock_requests_get):
         mock_dict = None
         mock_response = MockResponse(mock_dict, 200)
