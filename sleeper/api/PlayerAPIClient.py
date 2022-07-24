58308e8ce4652aec0b253538d41de1497b37d8d0
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-23 21:58:38 -0500

    cleanup

--- sleeper/api/PlayerAPIClient.py
+++ sleeper/api/PlayerAPIClient.py
@@ -6,6 +6,8 @@ from sleeper.model.PlayerTrend import PlayerTrend
 
 
 class PlayerAPIClient(SleeperAPIClient):
+    __DEFAULT_TRENDING_PLAYERS_LOOKBACK_HOURS = 24
+    __DEFAULT_TRENDING_PLAYERS_LIMIT = 25
 
     @classmethod
     def get_all_players(cls, *, sport: Sport) -> dict[str, Player]:
@@ -17,8 +19,8 @@ class PlayerAPIClient(SleeperAPIClient):
 
     @classmethod
     def get_trending_players(cls, *, sport: Sport, trend_type: TrendType, **kwargs) -> list[PlayerTrend]:
-        lookback_hours = kwargs.pop("lookback_hours", 24)
-        limit = kwargs.pop("limit", 25)
+        lookback_hours = kwargs.pop("lookback_hours", cls.__DEFAULT_TRENDING_PLAYERS_LOOKBACK_HOURS)
+        limit = kwargs.pop("limit", cls.__DEFAULT_TRENDING_PLAYERS_LIMIT)
         url = cls._build_route(cls._SLEEPER_APP_BASE_URL, cls._VERSION, cls._PLAYERS_ROUTE, sport.name.lower(),
                                cls._TRENDING_ROUTE, trend_type.name.lower())
         url = cls._add_filters(url, ("lookback_hours", lookback_hours), ("limit", limit))
