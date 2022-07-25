b6f1fe5095f7067c87eecca8be6220f8279e01b4
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-24 19:46:43 -0500

    added example file

---
+++ example/avatar_example.py
@@ -0,0 +1,9 @@
+from sleeper import AvatarAPIClient
+
+if __name__ == "__main__":
+    # get avatar by ID and save locally
+    AvatarAPIClient.get_avatar(avatar_id="my_avatar_id", save_to_path="C:\\Desktop\\avatar\\my_avatar.png")
+
+    # can pass in the "thumbnail" parameter to get a smaller-sized avatar
+    AvatarAPIClient.get_avatar(avatar_id="my_avatar_id", save_to_path="C:\\Desktop\\avatar\\my_avatar.png",
+                               thumbnail=True)
