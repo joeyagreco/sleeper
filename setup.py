f3bdca1172e87167a01738327d029f28317258d7
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-30 00:52:27 -0500

    updated version flow

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 import setuptools
 
+from _version import __version__ as version
+
 with open("README.md") as f:
     read_me = f.read()
 
 with open("LICENSE") as f:
-    license = f.read()
+    license_ = f.read()
 
 setuptools.setup(
     name="sleeper",
-    version="1.0.0",
+    version=version,
     author="Joey Greco",
     author_email="joeyagreco@gmail.com",
     description="A Python wrapper for the Sleeper API.",
     long_description_content_type="text/markdown",
     long_description=read_me,
-    license=license,
+    license=license_,
     include_package_data=True,
     packages=setuptools.find_packages(exclude=("test", "docs")),
     install_requires=["requests",
