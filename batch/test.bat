f79fba4a4f1a241376603f6e7e1f3f13d04581bf
jg <57115019+joeyagreco@users.noreply.github.com>
2023-10-10 10:04:58 -0500

    Updated dev flow.

--- batch/test.bat
+++ batch/test.bat
@@ -1,4 +1,21 @@
 @echo off
 
-cd test
-pytest
+IF "%1"=="fmt" (
+    :: Format the code using Black and Autoflake
+    black --config=pyproject.toml .
+    autoflake --config=pyproject.toml .
+) ELSE IF "%1" == "test" (
+    :: Run tests using pytest
+    cd test
+    pytest
+    cd ..
+) ELSE IF "%1"=="cov" (
+    :: Generate code coverage
+    cd test
+    coverage run -m pytest
+    coverage xml -o coverage.xml
+    coverage html
+    cd ..
+) ELSE (
+    echo Invalid flag. Please specify a valid flag.
+)
