2fc95fe0041b8d7ffea40730fe420ee43b6ec326
joeyagreco <joeyagreco@gmail.com>
2024-08-28 16:52:42 -0400

    test fixes

--- test/integration/test_api/test_draft.py
+++ test/integration/test_api/test_draft.py
@@ -2,16 +2,21 @@ import unittest
 
 from sleeper.api.draft import get_drafts_in_league, get_user_drafts_for_year
 from sleeper.enum.Sport import Sport
-from test.integration.test_api.constants import TEST_LEAGUE_ID, TEST_USER_ID
+from test.integration.test_api.constants import (
+    TEST_DRAFTS_FOR_LEAGUE_A,
+    TEST_DRAFTS_FOR_USER_A,
+    TEST_LEAGUE_ID_FOR_LEAGUE_A,
+    TEST_USER_ID_FOR_USER_A,
+)
 
 
 class TestDraft(unittest.TestCase):
     def test_get_user_drafts_for_year_happy_path(self):
         response = get_user_drafts_for_year(
-            user_id=TEST_USER_ID, sport=Sport.NFL, year="2023"
+            user_id=TEST_USER_ID_FOR_USER_A, sport=Sport.NFL, year="2023"
         )
-        self.assertEqual(4, len(response))
+        self.assertEqual(TEST_DRAFTS_FOR_USER_A, response)
 
     def test_get_drafts_in_league(self):
-        response = get_drafts_in_league(league_id=TEST_LEAGUE_ID)
-        self.assertEqual(1, len(response))
+        response = get_drafts_in_league(league_id=TEST_LEAGUE_ID_FOR_LEAGUE_A)
+        self.assertEqual(TEST_DRAFTS_FOR_LEAGUE_A, response)
--- test/integration/test_api/test_league.py
+++ test/integration/test_api/test_league.py
@@ -1,10 +1,13 @@
 import unittest
 
 from sleeper.api.league import get_league
-from test.integration.test_api.constants import TEST_LEAGUE_ID, TEST_LEAGUE_OBJECT
+from test.integration.test_api.constants import (
+    TEST_LEAGUE_ID_FOR_LEAGUE_A,
+    TEST_LEAGUE_OBJECT_FOR_LEAGUE_A,
+)
 
 
 class TestLeague(unittest.TestCase):
     def test_get_league(self):
-        response = get_league(league_id=TEST_LEAGUE_ID)
-        self.assertEqual(TEST_LEAGUE_OBJECT, response)
+        response = get_league(league_id=TEST_LEAGUE_ID_FOR_LEAGUE_A)
+        self.assertEqual(TEST_LEAGUE_OBJECT_FOR_LEAGUE_A, response)
