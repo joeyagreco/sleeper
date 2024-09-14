071b0bc52c4ecc86d1cb24f68e0c13beb56d1fd1
joeyagreco <joeyagreco@gmail.com>
2024-09-14 09:57:09 -0400

    added integration test

--- test/integration/test_api/test_league.py
+++ test/integration/test_api/test_league.py
@@ -1,6 +1,6 @@
 import unittest
 
-from sleeper.api.league import get_league, get_user_leagues_for_year
+from sleeper.api.league import get_league, get_rosters, get_user_leagues_for_year
 from sleeper.enum import Sport
 from test.integration.test_api.constants import (
     LEAGUE_A_LEAGUE_1,
@@ -20,3 +20,12 @@ class TestLeague(unittest.TestCase):
             user_id=USER_A_USER_ID, sport=Sport.NFL, year=2022
         )
         self.assertEqual(USER_A_LEAGUES_2022, response)
+
+
+    def test_get_rosters(self):
+        response = get_rosters(league_id=LEAGUE_A_LEAGUE_ID_1)
+        # this response will constantly change, so just assert some general things
+        self.assertIsInstance(response, list)
+        self.assertEqual(6, len(response))
+        for item in response:
+            self.assertIsInstance(item, dict)
