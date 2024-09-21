dfcb57ba948ff382376906d74b7303f33332d37a
joeyagreco <joeyagreco@gmail.com>
2024-09-21 12:22:18 -0400

    tests and stuff

--- sleeper/api/_utils.py
+++ sleeper/api/_utils.py
@@ -2,8 +2,6 @@ from typing import Any
 
 import requests
 
-# TODO: add tests for these
-
 
 def build_route(base_url: str, *paths: str | int) -> str:
     if base_url.endswith("/"):
@@ -12,6 +10,19 @@ def build_route(base_url: str, *paths: str | int) -> str:
     paths = tuple(str(p).strip("/") for p in paths)
     url = f"{base_url}/{'/'.join(paths)}"
     return url.strip("/")
+
+
+def add_filters(url: str, *args: tuple[str, Any]) -> str:
+    """
+    Adds filters to the given url.
+    """
+    symbol = "?"
+    if "?" in url:
+        symbol = "&"
+    for arg in args:
+        url = f"{url}{symbol}{arg[0]}={arg[1]}"
+        symbol = "&"
+    return url
 
 
 def get(url: str) -> Any:
@@ -24,19 +35,3 @@ def get_content(url: str) -> bytes:
     response = requests.get(url)
     response.raise_for_status()
     return response.content
-
-
-def add_filters(url: str, *args: tuple[str, Any]) -> str:
-    """
-    Adds filters to the given url.
-    """
-    symbol = "?"
-    if "?" in url:
-        symbol = "&"
-    for arg in args:
-        if arg[0] is not None and arg[1] is not None:
-            url = f"{url}{symbol}{arg[0]}={arg[1]}"
-        else:
-            raise ValueError("filters not formatted correctly")
-        symbol = "&"
-    return url
--- test/unit/test_api/test_utils.py
+++ test/unit/test_api/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from sleeper.api._utils import build_route
+from sleeper.api._utils import add_filters, build_route
 
 
 class TestUtils(unittest.TestCase):
@@ -19,3 +19,11 @@ class TestUtils(unittest.TestCase):
     def test_build_route_ending_path_ends_with_slash(self):
         resp = build_route("https://foo/", "bar", "/baz/")
         self.assertEqual("https://foo/bar/baz", resp)
+
+    def test_add_filters_no_filters_given(self):
+        resp = add_filters("https://foo")
+        self.assertEqual("https://foo", resp)
+
+    def test_add_filters_filters_given(self):
+        resp = add_filters("https://foo", ("bar", "baz"), ("qux", 1))
+        self.assertEqual("https://foo?bar=baz&qux=1", resp)
